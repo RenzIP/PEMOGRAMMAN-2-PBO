ae0a949a150582576b9a9ff48a552e506b4aacd2
RenzIP <zenkun.enterkill13@gmail.com>
2025-12-15 15:05:22 +0700

    Push Pertemuan 10

---
+++ Pertemuan10/Pratikum/P9_714240042/controller/Koneksi.cs
@@ -0,0 +1,47 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace P9_714240042
+{
+    internal class Koneksi
+    {
+        string connectionString = "Server=139.59.111.220;Database=pemrog2ulbi;Uid=gasba;Pwd=-Kampret321";
+        MySqlConnection kon;
+
+        public void OpenConnection()
+        {
+            kon = new MySqlConnection(connectionString);
+            kon.Open();
+        }
+
+        public void CloseConnection()
+        {
+            kon.Close();
+        }
+
+        public object ShowData(string query)
+        {
+            var table = new DataTable();
+            using (var conn = new MySqlConnection(connectionString))
+            {
+                conn.Open();
+                using (var adapter = new MySqlDataAdapter(query, conn))
+                {
+                    adapter.Fill(table);
+                }
+            }
+            return table;
+        }
+
+        public void ExecuteQuery(MySqlCommand command)
+        {
+            command.Connection = kon;
+            command.ExecuteNonQuery();
+        }
+    }
+}
